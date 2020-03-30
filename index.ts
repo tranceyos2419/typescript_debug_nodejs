ef01f3d2e504fbc4b80743bb1a17634e02be2484
tranceyos2419 <31304661+tranceyos2419@users.noreply.github.com>
2020-03-30 20:56:07 +0900

    build: successfully debug index.ts

--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
-console.log('fdfsfsf')
+
+
+
+const test = (a: number, b: number) => {
+  const c = a + b;
+  return c;
+}
+
+const m = test(1, 2)
+console.log('m:', m)
---
+++ out/index.js
@@ -0,0 +1,8 @@
+"use strict";
+var test = function (a, b) {
+    var c = a + b;
+    return c;
+};
+var m = test(1, 2);
+console.log('m:', m);
+//# sourceMappingURL=index.js.map
